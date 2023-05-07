c85cd935e995eba37b8f3f6ac98dd94fadf89966
Unashe Siwawa <anelesiwawa@gmail.com>
2023-05-07 21:45:08 +0200

    Update
    -- update

--- lib/components/form_location_picker.dart
+++ lib/components/form_location_picker.dart
@@ -1,23 +1,56 @@
-import 'dart:io';
-
-import 'package:digital_data_tree/components/app_buttons.dart';
-import 'package:file_picker/file_picker.dart';
-import 'package:flutter/services.dart';
+import 'package:digital_data_tree/app/app_const.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_colorpicker/flutter_colorpicker.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:map_location_picker/map_location_picker.dart';
+import 'package:open_street_map_search_and_pick/open_street_map_search_and_pick.dart';
 
-import '../app/app_const.dart';
+import '../app/location_services.dart';
 
-class FormLocationPicker {
-  static locationPicker(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: AppButton.normalButton(
-        title: 'Pick Location',
-        onPress: () async {},
+class FormLocationPicker extends StatefulWidget {
+  const FormLocationPicker({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<FormLocationPicker> createState() => _FormLocationPicker();
+}
+
+class _FormLocationPicker extends State<FormLocationPicker> {
+  final LocationService locationService = LocationService();
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+        body: SizedBox(
+      height: size.height,
+      width: size.width,
+      child: OpenStreetMapSearchAndPick(
+        locationPinIconColor: AppColors.primaryColor,
+        center: LatLong(-17.824858, 31.053028),
+        buttonColor: AppColors.primaryColor,
+        buttonText: 'Set Location',
+        onPicked: (pickedData) {
+          print(pickedData.latLong.latitude);
+          print(pickedData.latLong.longitude);
+          print(pickedData.address);
+        },
+        onGetCurrentLocationPressed: locationService.getPosition,
       ),
-    );
+    ));
   }
 }
--- lib/screens/form_detail_screen/form_detail_screen.dart
+++ lib/screens/form_detail_screen/form_detail_screen.dart
@@ -7,9 +7,15 @@ class FormDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: ListView(
-        children: [FormLocationPicker.locationPicker(context)],
+        children: const [
+          SizedBox(
+              height: 400,
+              width: 150.0,
+              child: FormLocationPicker(title: "Map"))
+        ],
       ),
     );
   }
